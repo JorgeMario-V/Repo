55f1d0df3e3daa0f0e2a577f1d4e25ad49635ded
Your Name <you@example.com>
2025-03-09 20:46:37 -0500

    Versión de prueba

---
+++ src/main/java/com/example/prueeba/PrueebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.prueeba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrueebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrueebaApplication.class, args);
+    }
+
+}
